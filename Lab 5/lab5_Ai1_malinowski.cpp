aff12d288da4fbd1af631432b778789adad08c9b
Michal6006 <michal.malinowski57@student.qcc.cuny.edu>
2026-02-05 04:16:21 +0000

    Lab 5

--- Lab 5/lab5_Ai1_malinowski.cpp
+++ Lab 5/lab5_Ai1_malinowski.cpp
@@ -2,7 +2,7 @@
 Michal Malinowski
 Feb 4, 2026
 Lab 5, Leveraging AI as a C++ Coding Assistant
-Git Hub Copiolot
+Git Hub Copilot
 */
 
 // code
@@ -42,3 +42,20 @@ int main() {
 
     return 0;
 }
+// --- AI Assistant Used: Git Hub Copilot ---
+// Q1) Was the code correct? (Yes/No and explain your answer)
+// Yes the code was correct, all of the tasks of the exercise were met but, the last part does not specify if the the entered word should be updated or not between the last 2 problems. 
+// Q2) Was it readable and properly commented? explain your answer
+// Yes is it readable and properly commented but the syntax could have been a little optimized by adding the command 'using namespace std;' at the start would save you the time of adding 'std::' in front of all the lines.
+// Q3) Were you able to explain each line of code? explain your answer
+// Yes I understand each line of the code it was very well constructed and the ai even went to the lengths to add failsafes in case the entered word would not be able to be altered by the if statements.
+// Q4) Did it use the same programming concepts we learned in class? Explain which concepts were used and which ones were unfamiliar. 
+// No and Yes. Since I am doing this lab a little late I learned about the if statements on the day after this lab was due. If I were to have done this lab when it was assigned I would have been a little unfamilair with the if and else statments in C++ but not completly lost because the if statements do share some charactersitics from phyton which I already took a course on. However, the size command is new to me.
+// Q5) Terminal Output: (Paste the output of your program here)
+/*  
+Type a word: Malinowski
+2nd character:  a
+word has:       10 characters
+Replace word:   Ma-- $ --nowski
+Remove end characters:  Malinow
+*/
--- Lab 5/lab5_Ai2_malinowski.cpp
+++ Lab 5/lab5_Ai2_malinowski.cpp
@@ -47,3 +47,20 @@ int main() {
 
     return 0;
 }
+// --- AI Assistant Used: ChatGPT ---
+// Q1) Was the code correct? (Yes/No and explain your answer)
+// Yes the code was correct, all of the tasks of the exercise were met but,  same as the last example  it does not specify if the the entered word should be updated or not between the last 2 problems. 
+// Q2) Was it readable and properly commented? explain your answer
+// ChatGPT's output was easier to read than the Git Hub Copilot one due to the optimized syntax, ChatGPT also gave more thorough comments for each step.
+// Q3) Were you able to explain each line of code? explain your answer
+// Yes every line was brief and commented well making it easy to read, Also, every printed line gave enough information to know what was going on if you skipped to that part of the code. In addition, I am familiar with all of the commands used.
+// Q4) Did it use the same programming concepts we learned in class? Explain which concepts were used and which ones were unfamiliar.
+// Same as the previous example No and Yes. Since I am doing this lab a little late I learned about the if statements on the day after this lab was due. If I were to have done this lab when it was assigned I would have been a little unfamilair with the if and else statments in C++ but not completly lost because the if statements do share some charactersitics from phyton which I already took a course on. However, none of the commands in this code were completly new to me. 
+// Q5) Terminal Output: (Paste the output of your program here)
+/*
+Enter a word: Malinowski
+2nd character: a
+word has: 10 characters
+Replace word: Ma--$--nowski
+Remove end characters: Malinow
+*/
