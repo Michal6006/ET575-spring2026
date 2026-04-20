f22179e0724cbc4d5e2b5ff3a6795c11cfa86ed0
Michal6006 <michal.malinowski57@student.qcc.cuny.edu>
2026-04-20 15:37:20 +0000

    Lab 18

---
+++ Lab 18/lab18_function_malinowski.cpp
@@ -0,0 +1,66 @@
+/*
+Michal Malinowski
+April 20, 2026
+Lab 18, Array Application
+*/
+#include<iostream>
+using namespace std;
+// Example 1: Search Program
+// Function 1: Function to collect up to 5 consecutive positive integers
+// The collection will stop if a negative number is entered.
+// If there is less than five numbers collected, the function will refrence to the array
+void fillarray(int *arr, int &numberuserindex, int arraysize){
+    int number = 0, index = 0;
+    do{
+        cout<<"Enter a positive number: ";
+        cin>>number;
+        if(number>0){
+            arr[index] = number;
+            index ++;
+        }
+    }while(number>0 && index<arraysize);
+
+    // Update the index of the last consecitove positvie number
+    numberuserindex = index;
+}
+// Function 2: Print each element in an array
+void printelement(int *arr, int sizearray){
+    for(int i = 0; i<sizearray; i++){
+        cout<<arr[i]<<"\t";
+    }
+    cout<<endl;
+}
+
+// Function 3: Search Function
+int search(int *arr, int numberuserindex, int target){
+    int index = 0;
+    bool found = false;
+
+    while(!found && index<numberuserindex){
+        if(target == arr[index])
+            found = true;
+        else
+            index++;
+    }
+
+    if(found)
+        return index;
+    else
+        return -1;
+
+}
+
+// Exercise
+void collectnumber(int *arr, int numberuserindex, int arraysize){
+    int number = 0, index = 0;
+    do{
+        cout<<"Enter a non-zero number: ";
+        cin>>number;
+        if(number>0 || number<0){
+            arr[index] = number;
+            index ++;
+        }
+    }while((number>0 || number<0) && index<arraysize);
+}
+
+float Averagenumber()
---
+++ Lab 18/lab18_main_malinowski.cpp
@@ -0,0 +1,42 @@
+/*
+Michal Malinowski
+April 20, 2026
+Lab 18, Array Application
+*/
+
+#include<iostream>
+using namespace std;
+
+#include"lab18_function_malinowski.cpp"
+
+int main(){
+    cout<<"\n----- Example 1: Search Function -----"<<endl;
+    // Declare the array size
+    const int ARRAYSIZE = 5;
+    // Declare the array
+    int a[ARRAYSIZE];
+    // Declare the array size if there is less than five consecutive positive numbers
+    int listsize;
+    // Decalre the variable to save target
+    int searchnumber = 20;
+
+    // Call function fillarray
+    fillarray(a, listsize, ARRAYSIZE);
+    // Testing listsize
+    cout<<listsize<<endl;
+
+    // Call function 2
+    printelement(a, listsize);
+
+    // Call function 3
+    int foundindex = search(a, listsize, searchnumber);
+    cout<<"Test search index "<<foundindex<<endl;
+
+    cout<<"\n----- Exercise -----"<<endl;
+    const int a_size = 5;
+    int ex_a[a_size];
+    int l_size;
+    collectnumber(ex_a, l_size, a_size);
+    return 0;
+}
+
